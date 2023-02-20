763ed4766f4a9a18f2e7451ec98d09796fdfee70
raufsamestone <berkaydemirbas@gmail.com>
2023-02-20 19:41:22 +0300

    added TFJS

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <Fragment>
       <Head>
-        <title>Audience Engine</title>
+        <title>🔮 Audience Engine</title>
       </Head>
 
       <div className="flex h-screen bg-gray-50 shadow-sm">
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
           <div className="h-0 flex-1 flex flex-col overflow-y-auto">
             <div className="flex-shrink-0 px-4 py-3 ">
               <h1 className="text-lg font-bold text-gray-900">
-                Audience Engine
+                🔮 Audience Engine
               </h1>
             </div>
             <nav className="flex-1 px-2 py-4 bg-white">
@@ -76,6 +76,26 @@ const Layout = ({ children }) => {
                   />
                 </svg>
                 Audience Merger
+              </a>
+              <a
+                href="/page-prediction"
+                className="group flex items-center px-2 py-2 text-sm font-medium rounded-md text-gray-700 bg-gray-100 hover:text-gray-900 hover:bg-gray-100"
+              >
+                <svg
+                  className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"
+                  />
+                </svg>
+                Page Prediction
               </a>
             </nav>
           </div>
